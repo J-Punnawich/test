f76fb570fe4e7fe6713fef9ab92d7e912908fec1
J-Punnawich <newandovergame@gmail.com>
2023-05-25 01:42:27 +0700

    rev dotenv

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 var express = require("express");
 var cors = require("cors");
 var app = express();
-require("dotenv").config;
 const mysql = require("mysql2");
 
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
 });
 
 app.use(cors());
 
-app.get("/products/:id", function (req, res, next) {
+app.get("/products", function (req, res, next) {
   res.json({ msg: "This is CORS-enabled for all origins!" });
 });
 
-app.get("/products", function (req, res, next) {
+app.get("/attractions", function (req, res, next) {
   pool.query("SELECT * FROM attractions", function (err, rows, field) {
     res.json(rows);
   });
